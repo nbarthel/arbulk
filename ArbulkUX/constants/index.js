8df1ef7ac3802f80a91c1ea4ba182acd36ec3877
naisergic <naisergic@6f65fa22-197a-42bb-9c58-d64d029bdc34>
2017-08-21 09:45:57 +0000

    Bug fix

--- ArbulkUX/constants/index.js
+++ ArbulkUX/constants/index.js
@@ -11,12 +11,7 @@
  *
  */
   export const WELCOME_TEXT = 'AR Bulk Withand Node loopback';
-  export const Base_Url = 'http://localhost:3000/api/'
+  //export const Base_Url = 'http://localhost:3000/api/'
 
- //export const Base_Url = 'http://192.168.0.105:8080/api/';
-//export const Base_Url = "http://172.27.101.87:8080/api/";
-  //export const Base_Url = "http://172.31.98.51:8080/api/";
+  export const Base_Url = 'http://13.126.216.105/api/'
 
-//export const Base_Url = "http://192.168.43.135:8080/api/"
-//export const Base_Url = "http://192.168.0.7:8080/api/"
-	//export const Base_Url = "http://10.0.0.4:8080/api/"
