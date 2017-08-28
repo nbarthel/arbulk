841503a754dd9bbca1035f65a96442ed8ba5e853
naisergic <naisergic@6f65fa22-197a-42bb-9c58-d64d029bdc34>
2017-08-28 05:37:09 +0000

    on grouping

--- ArbulkApi/common/models/t-company.js
+++ ArbulkApi/common/models/t-company.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = function(Tcompany) {
+    Tcompany.getCustomers = function (cb) {
+        var ds = Tcompany.dataSource;
+        var sql ="select count(aT.customer_id) as mostActive, bT.*, customer_id from  t_packaging_instructions as aT inner join t_company as bT on bT.id = aT.customer_id group by aT.customer_id order by mostActive desc"
+        ds.connector.query(sql, function (err, result) {
+            if (err) {
+                logger.error(err)
+                return cb(err);
+            }
+            cb(null, result);
+        });
 
+    }
+    Tcompany.remoteMethod('getCustomers', {
+        description: 'get all customers',
+        returns: { type: 'object',root: true},
+        http: {path:"/getCustomers", verb: 'get'}
+    });
 };
